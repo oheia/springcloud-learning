1e874919c13159a528dd68820d6a29f974798c41
oheia <oheia2021@outlook.com>
2021-03-04 16:53:21 +0800

    eureka-server,eureka-client,eureka-security-server

---
+++ eureka-security-server/src/test/java/com/oheia/cloud/EurekaSecurityServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oheia.cloud;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaSecurityServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
