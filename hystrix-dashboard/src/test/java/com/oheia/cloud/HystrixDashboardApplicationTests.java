0d882b780f690d66758da42ca518e58179141027
oheia <oheia2021@outlook.com>
2021-03-15 19:41:12 +0800

    ─ hystrix-service -- hystrix服务调用测试服务 ├── turbine-service -- 聚合收集hystrix实例监控信息的服务 └── hystrix-dashboard -- 展示hystrix实例监控信息的仪表盘

---
+++ hystrix-dashboard/src/test/java/com/oheia/cloud/HystrixDashboardApplicationTests.java
@@ -0,0 +1,14 @@
+package com.oheia.cloud;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ turbine-service/src/test/java/com/oheia/cloud/TurbineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oheia.cloud;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurbineServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
